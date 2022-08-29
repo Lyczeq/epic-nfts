d05299f5313ff576188740604ab5f4aa3fd4bc16
Lyczeq <lyczeq@gmail.com>
2022-08-29 19:28:07 +0200

    Updated hardhat to use env variables to deploy

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,8 +1,14 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks:{
+    goerli: {
+      url: process.env.ALCHEMY_API_KEY,
+      accounts: [process.env.GOERLI_PRIVATE_KEY as string],
+    }
+  }
 };
 
 export default config;

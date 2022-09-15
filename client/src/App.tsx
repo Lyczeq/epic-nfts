37674376055b3f89473a25a8ce0b7db1d51ced39
Lyczeq <lyczeq@gmail.com>
2022-09-15 19:32:52 +0200

    Added initial useDapp setup and the connection button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,30 @@
+import { useEtherBalance, useEthers, useConfig } from '@usedapp/core';
+import { formatEther } from '@ethersproject/units';
+
 function App() {
-  return null;
+  const { activateBrowserWallet, account, deactivate, chainId } = useEthers();
+  const etherBalance = useEtherBalance(account);
+  const config = useConfig();
+
+  if (!config.readOnlyUrls?.[chainId!]) {
+    return <p>Please use either Mainnet or Goerli testnet.</p>;
+  }
+
+  return (
+    <div>
+      {!account && (
+        <button onClick={() => activateBrowserWallet()}>Connect</button>
+      )}
+      {account && <button onClick={() => deactivate()}>Disconnect</button>}
+      {etherBalance && (
+        <div className="balance">
+          <br />
+          Balance:
+          <p className="bold">{formatEther(etherBalance)}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { Mainnet, DAppProvider, Config, Goerli } from '@usedapp/core';
+import { getDefaultProvider } from 'ethers';
 import App from './App';
+
+const config: Config = {
+  readOnlyChainId: Mainnet.chainId,
+  readOnlyUrls: {
+    [Mainnet.chainId]: getDefaultProvider('mainnet'),
+    [Goerli.chainId]: getDefaultProvider('goerli'),
+  },
+};
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <DAppProvider config={config}>
+      <App />
+    </DAppProvider>
   </React.StrictMode>
 );
